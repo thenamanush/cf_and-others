74967d76cc66afc2a62672c4d08c28ed2f715915
Bashar Numan <nomanthenamanush@gmail.com>
2025-02-25 18:38:50 +0600

    new

---
+++ harbour/cf/rating_1000/beautiful_array.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9+7;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pie 2 * (acos(0.0))
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define nl '\n'
+#define lcm(a, b) (a * b) / (std::gcd<ll>(a, b))
+#define print(v)          \
+    for (auto e : v)      \
+        cout << e << " "; \
+    cout << endl;
+#define printp(v)    \
+    for (auto e : v) \
+        cout << e.first << " " << e.second << endl;
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define trav(a, x) for (auto &a : x)
+#define F first
+#define S second
+#define setbit(x) __builtin_popcount(x)
+#define sz(x) (int)(x).size()
+#define vi vector<long long>
+#define pi pair<int, int>
+#define even(n) if (n % 2 == 0)
+#define odd(n) if (n % 2 == 1)
+#define sp << " " <<
+
+#define alliswell                \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+void solve(){
+    ll n, k, b, s;
+    cin >> n >> k >> b >> s;
+
+    ll mn = k * b;
+    ll mx = mn + (k - 1) * n;
+    
+    if(s > mx || s < mn){
+        cout << -1 << nl;
+        return;
+    }
+    vector<ll> a(n, 0);
+    ll add = k - 1;
+    a[0] = min(s, (mn + add));
+    s -= min(s, (mn + add));
+    for(int i = 1; i < n; ++i){
+        if(s > 0){
+            a[i] = min(s, add);
+            s -= min(s, add);
+        }
+        else{
+            break;
+        }
+    }
+    print(a);
+}
+int main()
+{
+    alliswell
+
+    int t = 1; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
