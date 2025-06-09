ac25c60e5731c85e811ab4c5c3b2ffb1a23fd697
Bashar Numan <nomanthenamanush@gmail.com>
2025-06-09 15:32:09 +0600

    new

---
+++ harbour/contests/round_1029/c.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9+7;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pie 2 * (acos(0.0))
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define nl '\n'
+#define lcm(a, b) (a * b) / (std::gcd<ll>(a, b))
+#define print(v)          \
+    for (auto e : v)      \
+        cout << e << " "; \
+    cout << endl;
+#define printp(v)    \
+    for (auto e : v) \
+        cout << e.first << " " << e.second << endl;
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define trav(a, x) for (auto &a : x)
+#define F first
+#define S second
+#define setbit(x) __builtin_popcount(x)
+#define sz(x) (int)(x).size()
+#define vi vector<long long>
+#define pi pair<int, int>
+#define even(n) if (n % 2 == 0)
+#define odd(n) if (n % 2 == 1)
+#define sp << " " <<
+
+#define alliswell                \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+void solve()
+{
+    ll n; cin >> n;
+    vi a(n);
+    rep(i, n){
+        cin >> a[i];
+    }
+    set<ll> s,tmp,st;
+    ll ans = 1;
+    s.insert(a[0]);
+    for(int i = 1; i < n; ++i){
+        // temp
+        if(s.find(a[i]) == s.end()){
+            tmp.insert(a[i]);
+        }
+        else{
+            st.insert(a[i]);
+            if(s == st){
+                ans++;
+                for(auto & it : tmp){
+                    s.insert(it);
+                }
+                tmp.clear();
+                st.clear();
+            }
+        }
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    alliswell
+
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
