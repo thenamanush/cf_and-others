cf0ad72f7c296d7f1aa93667e5892aaa282ffdf4
Bashar Numan <basharmohammadnoman@gmail.com>
2025-06-26 12:18:17 +0600

    new

--- harbour/dart/hello.dart
+++ harbour/dart/hello.dart
@@ -1,12 +1,20 @@
 import 'dart:io';
 
-main(){
-    // print("hello world");
-    // var firstname = 'Bashar';
-    // String lastname = 'Numan';
-    // print(firstname + ' ' + lastname);
+main() {
+  // print("hello world");
+  // var firstname = 'Bashar';
+  // String lastname = 'Numan';
+  // print(firstname + ' ' + lastname);
 
-    stdout.writeln('what is your name : ?');
-    String name = stdin.readLineSync()!;
-    print('my name is $name');
+  stdout.writeln('what is your name : ?');
+  String name = stdin.readLineSync()!;
+  print('my name is $name');
+
+  // int num = int.parse(stdin.readLineSync()!);
+  // print("your age is $num");
+
+  print(name.codeUnits);
+
+  print(name.length);
+  print(name.toLowerCase());
 }
