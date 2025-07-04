4f31d6896b3988086512191228b755af6c0cede6
Bashar Numan <basharmohammadnoman@gmail.com>
2025-07-04 23:58:24 +0600

    assignment

---
+++ harbour/dart/assignment2.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+void even_odd() {
+  print('Enter a number:');
+  int num = int.parse(stdin.readLineSync()!);
+
+  if ((num & 1) == 0) {
+    print('even');
+  } else {
+    print('odd');
+  }
+}
+
+void vowel_consonent() {
+  print('enter a charecter : ');
+  String c = stdin.readLineSync()!;
+
+  if (c == 'a' ||
+      c == 'e' ||
+      c == 'i' ||
+      c == 'o' ||
+      c == 'u' ||
+      c == 'A' ||
+      c == 'E' ||
+      c == 'I' ||
+      c == 'O' ||
+      c == 'U') {
+    print('vowel');
+  } else {
+    print('consonant');
+  }
+}
+
+void pos_neg() {
+  int num = int.parse(stdin.readLineSync()!);
+  if (num < 0) {
+    print('negative');
+  } else if (num > 0) {
+    print('positive');
+  } else {
+    print('zero');
+  }
+}
+
+void name() {
+  int i = 5;
+  while (i-- > 0) {
+    print("Noman");
+  }
+}
+
+void main() {
+  // checking if number is even or odd
+  even_odd();
+  // checking vowel or consonent
+  vowel_consonent();
+  // checking value of number
+  pos_neg();
+  //printing name 100 times
+  name();
+}
