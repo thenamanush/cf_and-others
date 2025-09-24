3e66b8743b9b1aec9953a949feb5ff287794bcc1
thenamanush <nomanthenamanush@gmail.com>
2025-09-24 13:53:38 +0600

    may be a restart in cp

---
+++ harbour/contests/educational_182/a.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pie 2 * (acos(0.0))
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define nl '\n'
+#define lcm(a, b) (a * b) / (gcd<ll>(a, b))
+#define print(v)          \
+    for (auto e : v)      \
+        cout << e << " "; \
+    cout << endl;
+#define printp(v)    \
+    for (auto e : v) \
+        cout << e.first << " " << e.second << endl;
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define trav(a, x) for (auto &a : x)
+#define F first
+#define S second
+#define setbit(x) __builtin_popcount(x)
+#define sz(x) (int)(x).size()
+#define vi vector<long long>
+#define pi pair<int, int>
+#define even(n) if (n % 2 == 0)
+#define odd(n) if (n % 2 == 1)
+#define sp << " " <<
+
+#define alliswell                \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vi a(n + 1);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+    }
+
+    vi pfs(n + 1, 0);
+    for (int i = 1; i <= n; ++i)
+    {
+        pfs[i] = a[i] + pfs[i - 1];
+    }
+    ll rm1, rm2, rm3;
+    for (int i = 1; i <= n - 2; ++i)
+    {
+        for (int j = i + 1; j <= n - 1; ++j)
+        {
+            rm1 = pfs[i] % 3;
+            rm2 = (pfs[j] - pfs[i]) % 3;
+            rm3 = (pfs[n] - pfs[j]) % 3;
+            if (rm1 == rm2  && rm1 == rm3)
+            {
+                cout << i sp j << nl;
+                return;
+            }
+            if (rm1 != rm2 && rm1 != rm3 && rm2 != rm3)
+            {
+                cout << i sp j << nl;
+                return;
+            }
+        }
+    }
+    cout << 0 sp 0 << nl;
+}
+
+int main()
+{
+    alliswell
+
+        int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
---
+++ harbour/contests/educational_182/b.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pie 2 * (acos(0.0))
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define nl '\n'
+#define lcm(a, b) (a * b) / (gcd<ll>(a, b))
+#define print(v)          \
+    for (auto e : v)      \
+        cout << e << " "; \
+    cout << endl;
+#define printp(v)    \
+    for (auto e : v) \
+        cout << e.first << " " << e.second << endl;
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define trav(a, x) for (auto &a : x)
+#define F first
+#define S second
+#define setbit(x) __builtin_popcount(x)
+#define sz(x) (int)(x).size()
+#define vi vector<long long>
+#define pi pair<long long, long long>
+#define even(n) if (n % 2 == 0)
+#define odd(n) if (n % 2 == 1)
+#define sp << " " <<
+
+#define alliswell                \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+void solve()
+{
+    ll n, zcnt = 0;
+    cin >> n;
+    vi a(n);
+    set<ll> st;
+    rep(i, n)
+    {
+        cin >> a[i];
+        if (a[i] != 0)
+            st.insert(a[i]);
+            else
+                zcnt++;
+    }
+
+    ll l = n;
+    for (int i = 0; i < n; ++i)
+    {
+        if (a[i] != i + 1)
+        {
+            if (a[i] != 0) // not 0
+            {
+                l = i;
+                break;
+            }
+            else // if 0
+            {
+                if (st.find(i + 1) != st.end()) // found
+                {
+                    l = i;
+                    break;
+                }
+                else // not found
+                {
+                    if(zcnt > 1){
+                        l = i;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    ll r = n;
+    for(int i = n - 1; i >= 0; --i){
+        if(a[i] != i + 1){
+            if(a[i] != 0){ // not 0
+                r = i;
+                break;
+            }
+            else{ // if 0
+                if(st.find(i + 1) != st.end()){ // found
+                    r = i;
+                    break;
+                }
+                else{ // not found
+                    if(zcnt > 1){
+                        r = i;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if(l == r){
+        cout << 0 << nl;
+        return;
+    }
+    cout << max(0LL, r - l + 1) << nl;
+}
+
+int main()
+{
+    alliswell
+
+        int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
